4139d8126bb15466863b9869cd98ca0871317c61
potatoGavin <mustv587@126.com>
2021-02-23 18:24:32 +0800

    creat project

---
+++ src/main/java/com/hemily/hsaasotanormal/HsaasOtanormalApplication.java
@@ -0,0 +1,13 @@
+package com.hemily.hsaasotanormal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HsaasOtanormalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HsaasOtanormalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hemily/hsaasotanormal/HsaasOtanormalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hemily.hsaasotanormal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HsaasOtanormalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
